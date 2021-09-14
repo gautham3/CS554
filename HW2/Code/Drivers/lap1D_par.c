f4b01459f09afb07bc328661e179d1f3a615f8c7
gautham3 <gautham3@illinois.edu>
2021-09-14 17:33:31 -0500

    looping over array sizesto time

--- HW2/Code/Drivers/lap1D_par.c
+++ HW2/Code/Drivers/lap1D_par.c
@@ -10,24 +10,31 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
 
-    int N = atoi(argv[1]);  // size of full 1-D problem
-    int ntimer = atoi(argv[2]); // No. of times lap1D solve done- time will be averaged
-    int itermax = 10000; // No. of iterations for each solve
-    int n = 0;
-    int* offsvec =  row_load_allot(N,numprocs+1); // vector of offsets for each worker (here master also works)
-    if (rank>=0) { n = offsvec[rank+1]-offsvec[rank]; } //Calc. no. of elements of u this worker rank deals with
+    int maxpow = atoi(argv[1]); 
+    int minpow = 3;
+    for (int i=minpow; i<=maxpow; i++)
+    {   
+        int N = pow (2.0, i);  // size of full 1-D problem
+        int ntimer = atoi(argv[2]); // No. of times lap1D solve done- time will be averaged
+        int itermax = 10000; // No. of iterations for each solve
+        int n = 0;
+        int* offsvec =  row_load_allot(N,numprocs+1); // vector of offsets for each worker (here master also works)
+        if (rank>=0) { n = offsvec[rank+1]-offsvec[rank]; } //Calc. no. of elements of u this worker rank deals with
 
-    float* u_b = create1dRandRHS(n);
-    float* u = create1dRandRHS(n);
+        float* u_b = create1dRandRHS(n);
+        float* u = create1dRandRHS(n);
 
-    // create struct with data for parallel routines
-    struct par_dat pmdat = par_struct_assign(offsvec,n,rank,N,numprocs); 
+        // create struct with data for parallel routines
+        struct par_dat pmdat = par_struct_assign(offsvec,n,rank,N,numprocs); 
 
-    //time 1D Laplacian solves
-    lap1D_MPI_timer_output(ntimer,pmdat,u,u_b,itermax);
+        //time 1D Laplacian solves
+        lap1D_MPI_timer_output(ntimer,pmdat,u,u_b,itermax);
 
-    //print_vec(n,usol);print_vec(n,u_b);
-    free(u_b); free(u);
+        //print_vec(n,usol);print_vec(n,u_b);
+        free(u_b); free(u);
+
+    }
+
     MPI_Finalize();
     return 0;
 }
--- HW2/Code/runFolder/localRuns/lap1Dtimer_local.sh
+++ HW2/Code/runFolder/localRuns/lap1Dtimer_local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-N=8192	## Size of 1D data array
+N=10	## Size of max 1D data array=2^N
 ntimer=1000	## no. of 1D Lap solves done to avg time
 p=4 	## No. of MPI ranks to use
 
